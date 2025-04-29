ba6e14550e0ab179544c9efa65ad1a3cab54fb5c
Anton1o Vakhnin <anton1ovakhnin@Air-Anton1o.HomeLAN>
2025-04-29 17:15:43 +0300

    output 5 queries with a request for time and time zone name

--- first_script/main.py
+++ first_script/main.py
@@ -1,4 +1,6 @@
 import requests
+from datetime import datetime, timezone
+import pytz
 
 def request_to_the_server():
     url = 'https://yandex.com/time/sync.json?geo=213'
@@ -7,8 +9,27 @@ def request_to_the_server():
 
 def main():
     
-    data = request_to_the_server()
-    print(f"Заспрос:", data)
+	for _ in range(5):
+            
+		#start_time = datetime.now(timezone.utc)
+                
+		# вывод запроса
+		data = request_to_the_server()
+		print('\n')
+		print(f"Заспрос:\n", data)
+		#print(f"Время начальное:", start_time.strftime('%Y-%m-%d %H:%M:%S'))
+        
+		# преобразую время в секунды
+		timestamp = data['time'] / 1000
+        # время исходя из моей временной зоны
+		local_time = datetime.fromtimestamp(timestamp, timezone.utc).astimezone()
+		# получение названия временной зоны
+		timezone_name = local_time.astimezone(pytz.timezone('Europe/Moscow')).tzname()
+		print(f"Время начальное:, {local_time.strftime('%Y-%m-%d %H:%M:%S')} ({timezone_name})")
+
+            
+
+
    
 if __name__ == '__main__':
     main()
