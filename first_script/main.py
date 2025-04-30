f8bf9bbc565d7ba157d7c32d325fc32b124b2b74
Anton1o Vakhnin <anton1ovakhnin@Air-Anton1o.HomeLAN>
2025-04-30 13:42:43 +0300

    Added calculation of average time delta. Error handling with request

--- first_script/main.py
+++ first_script/main.py
@@ -2,38 +2,53 @@ import requests
 from datetime import datetime, timezone
 import pytz
 
-def request_to_the_server():
+def request_to_the_server(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()    
+        return response.json()
+    # Ошбика юрл
+    except requests.exceptions.MissingSchema:
+        raise ValueError(f"Неверный URL: {url}")
+    # Ошибка
+    except requests.exceptions.RequestException as e:
+        raise RuntimeError(f"Ошибка запроса: {e}")
+
+def main():  
     url = 'https://yandex.com/time/sync.json?geo=213'
-    response = requests.get(url)
-    return response.json()
+    deltas = []
 
-def main():
-      
-	deltas = []
+    for _ in range(5):
+        start_time = datetime.now(timezone.utc)
+
+        try:
+            # вывод запроса
+            data = request_to_the_server(url) 
+            print('\n')
+            print(f"Заспрос:\n", data)
     
-	for _ in range(5):
-            
-		start_time = datetime.now(timezone.utc)
-                
-		# вывод запроса
-		data = request_to_the_server()
-		print('\n')
-		print(f"Заспрос:\n", data)
-		#print(f"Время начальное:", start_time.strftime('%Y-%m-%d %H:%M:%S'))
+            # преобразую время в секунды
+            timestamp = data['time'] / 1000
+            # время исходя из моей временной зоны
+            local_time = datetime.fromtimestamp(timestamp, timezone.utc).astimezone()
+            # получение названия временной зоны
+            timezone_name = local_time.astimezone(pytz.timezone('Europe/Moscow')).tzname()
+            print(f"Время запроса: {local_time.strftime('%Y-%m-%d %H:%M:%S')} ({timezone_name})")
+    
+            # Вычисление дельты времени на запрос
+            end_time = datetime.now(timezone.utc)
+            delta = (end_time - start_time).total_seconds()
+            deltas.append(delta)
+            print(f"Дельта: {delta:.6f}")
         
-		# преобразую время в секунды
-		timestamp = data['time'] / 1000
-        # время исходя из моей временной зоны
-		local_time = datetime.fromtimestamp(timestamp, timezone.utc).astimezone()
-		# получение названия временной зоны
-		timezone_name = local_time.astimezone(pytz.timezone('Europe/Moscow')).tzname()
-		print(f"Время запроса: {local_time.strftime('%Y-%m-%d %H:%M:%S')} ({timezone_name})")
-
-        # Вычисление дельты времени на запрос
-		end_time = datetime.now(timezone.utc)
-		delta = (end_time - start_time).total_seconds()
-		deltas.append(delta)
-		print(f"Средняя дельта: {delta:.6f}")
+        except (ValueError, RuntimeError) as e:
+            print(e)
+            break
+            
+    # Вычисление средней дельты за 5 запросов
+    if deltas:
+        average_delta = sum(deltas) / len(deltas)
+        print(f"\nСредняя дельта: {average_delta:.6f}")
 
    
 if __name__ == '__main__':
