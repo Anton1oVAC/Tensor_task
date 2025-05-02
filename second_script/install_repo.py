984c22f58d1931722d48723bab4dde5ff1774063
Anton1o Vakhnin <anton1ovakhnin@Air-Anton1o.HomeLAN>
2025-05-02 10:57:09 +0300

    splitting code into files for better readability

---
+++ second_script/install_repo.py
@@ -0,0 +1,17 @@
+from log import logger
+import os
+import git
+
+# Клонирование репо
+def install_repo(repo_url, dest_dir):
+    
+    # Проверяем, существует ли директория
+    if os.path.exists(dest_dir):
+        logger.error(f"Директория {dest_dir} уже существует. Завершение.")
+        return None
+    
+    logger.info(f"Клонирование репозитория {repo_url}")
+    repo = git.Repo.clone_from(repo_url, dest_dir)
+    logger.info(f"Репозиторий успешно клонирован в {dest_dir}")
+    return repo
+
--- second_script/main.py
+++ second_script/main.py
@@ -1,22 +1,8 @@
-import git
 from log import logger
-import os
+from install_repo import install_repo
 
 repo_url = 'https://github.com/Anton1oVAC/s21_Matrix_oop'
 dest_dir = 's21'
-
-# Клонирование репо
-def install_repo(repo_url, dest_dir):
-    # Проверяем, существует ли директория
-    if os.path.exists(dest_dir):
-        logger.error(f"Директория {dest_dir} уже существует. Завершение.")
-        return None
-    
-    logger.info(f"Клонирование репозитория {repo_url}")
-
-    repo = git.Repo.clone_from(repo_url, dest_dir)
-    logger.info(f"Репозиторий успешно клонирован в {dest_dir}")
-    return repo
 
 
 def main():
